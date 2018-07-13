96389a0a3af2991f1dfa13bfc8c3c0290f85b4f0
Hareesh Chatarasupalli <32893983+hareeshrao4839@users.noreply.github.com>
2018-07-13 16:55:31 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -13,6 +13,13 @@ module.exports = function(config) {
     }
   })
 }
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
+module.exports = function(config) {
+  config.set({
+    browsers: ['ChromeHeadless']
+  })
+}
 
 module.exports = function (config) {
   config.set({
